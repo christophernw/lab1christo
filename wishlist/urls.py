a9deeff53920056c9fcb62d5bda687b0bfa0066b
Christopher Nathanael Wijaya <christophernathanael10@gmail.com>
2022-09-22 13:06:38 +0700

    lab 3 pbp

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from wishlist.views import show_wishlist
 from wishlist.views import show_xml,show_json,show_json_by_id
+from wishlist.views import register #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import logout_user #sesuaikan dengan nama fungsi yang dibuat
 
 app_name = 'wishlist'
 
@@ -9,5 +12,9 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'), #sesuaikan dengan nama fungsi yang dibuat
     path('json/', show_json, name='show_json'), #sesuaikan dengan nama fungsi yang dibuat
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'), #sesuaikan dengan nama fungsi yang dibuat
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
+    path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
+    path('logout/', logout_user, name='logout'), #sesuaikan dengan nama fungsi yang dibuat
+    
     #path('xml/<int:id>', show_json_by_id, name='show_json_by_id'), #sesuaikan dengan nama fungsi yang dibuat
 ]
